ddbbda3a2c26af3cc0e078ba0d5c9dec0861b208
shenyangHuang <shenyang.huang@mail.mcgill.ca>
2022-09-15 13:27:08 -0400

    added install.sh for easier installation

---
+++ install.sh
@@ -0,0 +1,8 @@
+conda install pytorch torchvision torchaudio cudatoolkit=11.3 -c pytorch
+conda install -c anaconda pandas
+conda install -c anaconda scikit-learn
+conda install -c conda-forge tqdm
+conda install -c anaconda numpy
+conda install -c conda-forge matplotlib
+
+
